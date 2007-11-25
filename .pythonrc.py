fa4b0932cf889bbf3a39778054a694cfd6223ef5
shouse <none@none>
2007-11-25 02:02:56 +0000

    Added pretty-printing.
    --HG--
    branch : homedir
    extra : convert_revision : svn%3A149dab3b-66d8-4c76-8704-0b19dd047065/homedir%40145

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,8 +1,9 @@
 """This file is executed when the Python interactive shell is started if
 $PYTHONSTARTUP is in your environment and points to this file. It's just
-regular Python commands, so do what you will!"""
+regular Python commands, so do what you will! Your ~/.inputrc file can greatly
+enhance this file."""
 
-import sys, os, readline, rlcompleter, atexit
+import sys, os, readline, rlcompleter, atexit, pprint
 
 GREEN = '\001\033[0:1;32m\002'
 RED = '\001\033[0:31m\002'
@@ -36,14 +37,17 @@ if os.environ.get( 'TERM' ) in [ 'xterm', 'xterm-color', 'xterm-256color', 'scre
     sys.ps1 = '%s>>> %s' % (GREEN, NORMAL)
     sys.ps2 = '%s... %s' % (RED, NORMAL)
 
+# Enable Pretty Printing for stdout
+sys.displayhook = pprint.pprint
+
 # Welcome message
 #################
 
 print """%(GREEN)s
 The Python shell is coming at 'ya, punk!
 %(RED)s
-You've got color, history, and completion
-(and vi-mode keybindings if your ~/.inputrc doesn't suck).
+You've got color, history, and pretty printing.
+(and completion and vi-mode keybindings if your ~/.inputrc doesn't suck).
 %(GREEN)s
 Oh yeah, it is that cool.
 %(NORMAL)s""" % locals()
