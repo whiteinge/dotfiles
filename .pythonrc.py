873553e862739dc458dc7066c2b66765472812f5
shouse <none@none>
2007-11-25 08:47:50 +0000

    Added better color support and pretty printing.
    --HG--
    branch : homedir
    extra : convert_revision : svn%3A149dab3b-66d8-4c76-8704-0b19dd047065/homedir%40146

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,25 +1,59 @@
-"""This file is executed when the Python interactive shell is started if
+"""Best goddamn .pythonrc file in the whole world.
+
+This file is executed when the Python interactive shell is started if
 $PYTHONSTARTUP is in your environment and points to this file. It's just
-regular Python commands, so do what you will! Your ~/.inputrc file can greatly
-enhance this file."""
+regular Python commands, so do what you will. Your ~/.inputrc file can greatly
+complement this file."""
 
-import sys, os, readline, rlcompleter, atexit, pprint
+AUTHOR = 'Seth House'
+CONTACT = 'seth@eseth.com'
+VERSION = '$Rev$'
+DATE = '$Date'
 
-GREEN = '\001\033[0:1;32m\002'
-RED = '\001\033[0:31m\002'
-NORMAL = '\001\033[0m\002'
+import sys, os, readline, rlcompleter, atexit, pprint, __builtin__
 
-HISTFILE="%s/.python-history" % os.environ["HOME"]
+# Color Support
+###############
 
-# Enable Completion & Readline Support
-######################################
+class TermColors(dict):
+    """Gives easy access to ANSI color codes. Attempts to fall back to no color
+    for certain TERM values. (Mostly stolen from IPython.)"""
 
-# These are better defined in your .inputrc
-# readline.parse_and_bind('tab: complete')
-# readline.parse_and_bind("tab: menu-complete")
+    COLOR_TEMPLATES = (
+        ("Black"       , "0;30"),
+        ("Red"         , "0;31"),
+        ("Green"       , "0;32"),
+        ("Brown"       , "0;33"),
+        ("Blue"        , "0;34"),
+        ("Purple"      , "0;35"),
+        ("Cyan"        , "0;36"),
+        ("LightGray"   , "0;37"),
+        ("DarkGray"    , "1;30"),
+        ("LightRed"    , "1;31"),
+        ("LightGreen"  , "1;32"),
+        ("Yellow"      , "1;33"),
+        ("LightBlue"   , "1;34"),
+        ("LightPurple" , "1;35"),
+        ("LightCyan"   , "1;36"),
+        ("White"       , "1;37"),
+        ("Normal"      , "0"),
+    )
+
+    NoColor = ''
+    _base  = '\001\033[%sm\002'
+
+    def __init__(self):
+        if os.environ.get('TERM') in ('xterm-color', 'xterm-256color', 'linux',
+                                    'screen', 'screen-256color', 'screen-bce'):
+            self.update(dict([(k, self._base % v) for k,v in self.COLOR_TEMPLATES]))
+        else:
+            self.update(dict([(k, self.NoColor) for k,v in self.COLOR_TEMPLATES]))
+_c = TermColors()
 
 # Enable a History
 ##################
+
+HISTFILE="%s/.python-history" % os.environ["HOME"]
 
 # Create the file if it doesn't yet exist
 if os.path.exists(HISTFILE):
@@ -33,21 +67,31 @@ atexit.register(savehist)
 # Enable Color Prompts
 ######################
 
-if os.environ.get( 'TERM' ) in [ 'xterm', 'xterm-color', 'xterm-256color', 'screen', 'screen-256color', 'screen-bce' ]:
-    sys.ps1 = '%s>>> %s' % (GREEN, NORMAL)
-    sys.ps2 = '%s... %s' % (RED, NORMAL)
+sys.ps1 = '%s>>> %s' % (_c['Green'], _c['Normal'])
+sys.ps2 = '%s... %s' % (_c['Red'], _c['Normal'])
 
 # Enable Pretty Printing for stdout
-sys.displayhook = pprint.pprint
+###################################
+
+def my_displayhook(value):
+    if value is not None:
+        __builtin__._ = value
+        pprint.pprint(value)
+sys.displayhook = my_displayhook
 
 # Welcome message
 #################
 
-print """%(GREEN)s
+print """%(Green)s
 The Python shell is coming at 'ya, punk!
-%(RED)s
+%(Cyan)s
 You've got color, history, and pretty printing.
-(and completion and vi-mode keybindings if your ~/.inputrc doesn't suck).
-%(GREEN)s
+(If your ~/.inputrc doesn't suck, you've also
+got completion and vi-mode keybindings.)
+%(Brown)s
 Oh yeah, it is that cool.
-%(NORMAL)s""" % locals()
+%(Normal)s""" % _c
+
+atexit.register(lambda: sys.stdout.write("""%(DarkGray)s
+I thought he'd never leave. Who invited that guy?
+%(Normal)s""" % _c))
