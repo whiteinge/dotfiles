92d503a7596a2ea61c84ad3cd26c1bfae8db364d
Seth House <seth@eseth.com>
2009-06-23 11:46:10 -0600

    Modified syntax for openbox-session instead of just openbox.
    --HG--
    branch : homedir

--- .openbox/autostart.sh
+++ .openbox/autostart.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
-xsetroot -solid "#303030"
-display -window root $HOME/.openbox/background.jpg
+# Run the system-wide support stuff
+. $GLOBALAUTOSTART
+
+xsetroot -solid "#303030" &
+display -window root $HOME/.openbox/background.jpg &
