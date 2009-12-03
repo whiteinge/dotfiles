dfd82afa19dc4b160f047b7f41f1df0581bba306
Seth House <seth@eseth.com>
2009-12-03 16:38:50 -0700

    Added (finally) a default pdb config.
    --HG--
    branch : homedir

---
+++ .pdbrc.py
@@ -0,0 +1,39 @@
+# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/498182
+def complete(self, text, state):
+    """return the next possible completion for text, using the current frame's
+       local namespace
+
+       This is called successively with state == 0, 1, 2, ... until it
+       returns None.  The completion should begin with 'text'.
+    """
+    # keep a completer class, and make sure that it uses the current local scope 
+    if not hasattr(self, 'completer'):
+        self.completer = rlcompleter.Completer(self.curframe.f_locals)
+    else:
+        self.completer.namespace = self.curframe.f_locals
+    return self.completer.complete(text, state)
+
+
+
+# Command line history:
+import readline
+histfile = os.path.expanduser("~/.pdb-pyhist")
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+import atexit
+atexit.register(readline.write_history_file, histfile)
+del histfile
+readline.set_history_length(1000)
+
+# return to debugger after fatal exception (Python cookbook 14.5):
+def info(type, value, tb):
+    if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+        sys.__excepthook__(type, value, tb)
+    import traceback, pdb
+    traceback.print_exception(type, value, tb)
+    print
+    pdb.pm()
+
+sys.excepthook = info
