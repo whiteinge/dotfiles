7043d6f23b4fc72a7aa1400815be8b05b26ea949
Seth House <seth@eseth.com>
2010-12-19 22:18:42 -0700

    Vastly improved the low-battery warning script
    Now can be called directly by acpid or as a stand-alone shell-script
    (e.g., via cron).

--- bin/warn-low-batt.sh
+++ bin/warn-low-batt.sh
@@ -1,25 +1,94 @@
-#!/bin/sh
+#!/usr/bin/env zsh
+# Output a warning when the battery drops below a certain percentage;
+# put the machine to sleep when the battery drops below a certain percentage.
+# vim:ft=zsh
+# #############################################################################
 
-if [ $# == 0 ]; then
-    echo "Usage:"
-    echo "$(basename $0) BAT1 ADP1"
-    exit 1;
-fi
+local NAME=$(basename $0)
 
-battery_id=$1
-ac_adapter_id=$2
-critical_level=10
+# Exit with an error
+function error() {
+    EXIT=$1 ; MSG=${2:-"$NAME: Unknown Error"}
+    [[ $EXIT -eq 0 ]] && echo $MSG || echo $MSG 1>&2
+    exit $EXIT
+}
 
-if [ "$(grep -o off /proc/acpi/ac_adapter/$ac_adapter_id/state)" == "off" ]; then
-    battery_max=`awk ' NR==3 {print $4}' /proc/acpi/battery/$battery_id/info`
-    battery_current=`awk ' NR==5 {print $3}' /proc/acpi/battery/$battery_id/state`
-    battery_level=$((100*$battery_current/$battery_max))
+# Send a graphical message about the current battery usage
+function message() {
+    local msg msgcmd user display
+    local -a curusers
+    msg=$1
 
-    if [ $battery_level -le $critical_level ]; then
-        if [[ -n "$(notify-send --help)" ]]; then
-            notify-send "Low battery warning!" "${battery_level}% remains." -t 0
+    if [[ -n $(which notify-send 2>/dev/null) ]] ; then
+        msgcmd=notify-send
+    elif [[ -n $(which xmessage 2>/dev/null) ]] ; then
+        msgcmd=xmessage
+    else
+        error 1 "No messaging command found."
+    fi
+
+    # Send a message to all users, if root, or current user
+    if [[ $EUID -eq 0 ]] ; then
+        curusers=( $(w -s -h | awk '/(startx|dm\?)/ { print $1, $2 }') )
+
+        for user display in $curusers; do
+            DISPLAY=$display sudo -u $user $msgcmd "${msg}"
+        done
+    else
+        $msgcmd "${msg}"
+    fi
+}
+
+function main() {
+    # NOTE: acpid starts emiting low-battery events at 5%
+    local -a OPTS
+    local WARNLEVEL=5
+    local SLEEPLEVEL=3
+    local BATT=$(acpi -b | awk '
+        BEGIN { FS=", " }
+
+        /Discharging/ {
+            sub(/\%/, "", $2)
+            print $2
+        }')
+    local HELPTEXT="\
+    Usage:: ${NAME} [options]
+
+    Options:
+    --warn=warn, -w warn
+        Set the battery percentage to begin displaying warnings.
+        Default: ${WARNLEVEL}
+    --sleep=sleep, -s sleep
+        Set the battery percentage at which to put the machine to sleep.
+        Default: ${SLEEPLEVEL}
+    --help
+        Print this summary and exit.
+    "
+
+    # Parse any arguments
+    # zparseopts -D -a OPTS -- \
+    #     -warn:=WARNLEVEL w:=WARNLEVEL \
+    #     -sleep:=SLEEPLEVEL s:=SLEEPLEVEL \
+    #     -help || exit 1
+
+    # Print help or version info
+    # (( $OPTS[(I)--help] )) && error 0 "${HELPTEXT}"
+
+    # Bail out early if connected to AC
+    [[ -n $BATT ]] || exit 0
+
+    # Emit warnings
+    if [[ $BATT -le $WARNLEVEL ]] ; then
+        if [[ $BATT -le $SLEEPLEVEL ]] ; then
+            message "Your battery is critical; sleeping..."
+            sleep 5
+            /etc/acpi/sleep.sh
         else
-            xmessage "Low Battery Warning! ${battery_level}% remains."
+            message "Your battery level is currently ${BATT}%"
         fi
     fi
-fi
+}
+
+# #############################################################################
+
+main $*
