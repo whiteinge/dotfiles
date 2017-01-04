18c5f58e00fc35410f31f6f6ea4743c2a1f82aa2
Seth House <seth@eseth.com>
2017-01-03 20:23:59 -0700

    Switch npm-init script to use TypeScript for transpilation

--- .npm-init.js
+++ .npm-init.js
@@ -7,7 +7,7 @@ module.exports = {
     private: package.private || prompt('private', 'false', JSON.parse),
     keywords: package.keywords || prompt('keywords', '', val =>
             val.split(' ').filter(String)),
-    main: prompt('main', package.main || 'index.js'),
+    main: prompt('main', package.main || 'lib/src/index.js'),
     repository: package.repository || prompt('repository', ''),
     homepage: package.homepage || prompt('homepage', '', val =>
         val !== '' ? val : `${module.exports.repository}#readme`),
@@ -15,7 +15,9 @@ module.exports = {
     dependencies: package.dependencies || {},
     optionalDependencies: package.optionalDependencies || {},
     devDependencies: package.devDependencies || {
-        'tape': '~4.6.0',
+        'tape': '4.x.x',
+        'typescript': '2.x.x',
+        'ts-node': '2.x.x',
     },
 
     eslintConfig: package.eslintConfig || {
@@ -27,25 +29,9 @@ module.exports = {
         },
     },
 
-    scripts: package.scripts || prompt('Babel?', 'true', function(val) {
-        if (JSON.parse(val) === false) {
-            return {
-                test: 'tape tests.js tests/**/*.js',
-            };
-        }
-
-        module.exports.main = 'dist/build.js';
-        module.exports.devDependencies['babel-preset-es2015'] = '~6.14.0';
-        module.exports.eslintConfig.parser = 'babel-eslint';
-        module.exports.eslintConfig.env['es6'] = true;
-        module.exports.eslintConfig.ecmaFeatures = {
-            modules: true,
-        };
-
-        return {
-            test: 'tape -r babel-register tests.js tests/**/*.js',
-            build: `browserify --standalone ${module.exports.name} -t rollupify -t babelify -t uglifyify index.js -o dist/build.js`,
-            preversion: 'npm run build',
-        };
-    }),
+    scripts: package.scripts || {
+        test: `ts-node -D -F -O '{\"allowJs\": true}' node_modules/tape/bin/tape tests/**/*.js`,
+        build: 'tsc --allowJs -m umd --outDir lib src/* tests/*',
+        preversion: 'npm run build',
+    },
 }
