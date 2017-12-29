cdd0ab2e639dddc75d45366d9c8a260f81d89a84
Seth House <seth@eseth.com>
2017-12-29 00:52:19 -0700

    Finalize browserify/node instant sandbox workflow

--- .npm-init.js
+++ .npm-init.js
@@ -1,6 +1,71 @@
+/**
+Quickly get a new project up and running
+
+Transpiles ES6 and produces Node and browser builds. Quick start:
+npm init -y
+npm i
+npm start
+
+Browserified sandbox:
+npm i rx
+echo "import * as Rx from 'rx'; window.Rx = Rx" > src/index.js
+npm run start:browser
+
+Node console sandbox:
+npm i rx
+echo "import * as Rx from 'rx'; console.log('rx', Rx);" > src/index.js
+npm run start:node
+**/
+
+const fs = require('fs');
+const path = require('path');
+
 const parseBool = str => ['y', 'yes'].includes(str.toLowerCase());
 const set = (name, def, infmt, outfmt = x => x) =>
     yes ? def : prompt(name, package[name] ? outfmt(package[name]) : def, infmt);
+
+const srcDir = 'src';
+const distDir = 'dist';
+const testDir = 'tests';
+
+const main = path.join(distDir, srcDir, 'index.js');
+const src = path.join(srcDir, 'index.js');
+const min = path.join(distDir, `${basename}.min.js`);
+
+const tscArgs = `--allowJs -t es5 -m commonjs --outDir ${distDir} ${srcDir}/* ${testDir}/*`;
+
+// Create .gitignore file.
+const gitignore = path.join(dirname, '.gitignore');
+if (!fs.existsSync(gitignore)) fs.writeFileSync(gitignore, `
+/dist
+/node_modules
+`);
+
+// Create HTML template.
+const index = path.join(dirname, 'index.tmpl');
+if (!fs.existsSync(index)) fs.writeFileSync(index, `
+<!doctype html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <title>${basename}</title>
+    <!-- inject:git-hash -->
+</head>
+<body>
+    <div id="container"></div>
+    <!-- inject:js -->
+    <!-- endinject -->
+</body>
+</html>
+`);
+
+// Scaffold common dirs.
+[srcDir, testDir].forEach(function(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+        fs.writeFileSync(path.join(dirname, dir, 'index.js'), '');
+    };
+});
 
 module.exports = {
     name: set('name', basename),
@@ -16,8 +81,7 @@ module.exports = {
     keywords: set('keywords',  '',
         val => val.split(',').filter(String).map(x => x.trim()),
         val => val.join(', ')),
-    main: 'dist/src/index.js',
-    browser: `dist/${basename}.min.js`,
+    main: main,
     repository: set('repository', '', x => x, x => x.url),
     homepage: set('homepage', ''),
 
@@ -26,12 +90,15 @@ module.exports = {
     devDependencies: package.devDependencies || {
         'browserify': '14.x.x',
         'json-server': '0.12.x',
+        'npm-run-all': '4.x.x',
+        'postbuild': '2.x.x',
         'shx': '0.2.x',
-        'tap-spec': '4.1.x',
+        'tap-spec': '4.x.x',
         'tape': '4.x.x',
         'ts-node': '2.x.x',
+        'ts-watch': '1.x.x',
         'typescript': '2.x.x',
-        'uglify-js': '2.8.x',
+        'uglify-js': '2.x.x',
     },
 
     eslintConfig: package.eslintConfig || {
@@ -44,17 +111,24 @@ module.exports = {
     },
 
     scripts: package.scripts || {
-        'build': 'NODE_ENV=production npm -s run build:tsc; NODE_ENV=production npm -s run build:browser',
-        'build:browser': cb => cb(null, `browserify ./dist/src/index.js | uglifyjs > ${module.exports.browser}`),
-        'build:tsc': 'tsc --allowJs -t es5 -m commonjs --outDir dist src/* tests/*',
-        'install:basedirs': 'shx mkdir -p dist src tests',
-        'install:basefiles': 'shx touch src/index.js tests/index.js',
-        'postinstall': 'npm run -s install:basedirs; npm run -s install:basefiles',
-        'preversion': 'npm run build',
-        'srv:local': 'json-server dist/sandbox-db.json --static .',
-        'test': 'npm -s run test:suite || EXIT=$? npm -s run test:lint || EXIT=$?; exit ${EXIT:-0}',
-        'test:lint': 'eslint ./src',
-        'test:suite': `npm -s run test:tape -- tests/**/*.js | tap-spec`,
-        'test:tape': `ts-node -D -F -O '{\"allowJs\": true}'`,
+        'build': `NODE_ENV=production run-s build:all`,
+        'build:all': `run-s build:ts build:browser`,
+        'build:browser': cb => cb(null, `browserify ${main} | uglifyjs > ${min}`),
+        'build:ts': `tsc ${tscArgs}`,
+        'log': `clear && node ${main}`,
+        'presrv:browser': `shx mkdir -p ${distDir}`,
+        'postbuild:browser': `postbuild -i index.tmpl -o ${distDir}/index.html -j ${min} -g ${distDir}`,
+        'preversion': `npm run build`,
+        'repl': `ts-node -D -F -O '{\"allowJs\": true}'`,
+        'srv:browser': `json-server ./${distDir}/${basename}-db.json --static ./${distDir}`,
+        'start': `run-s start:browser`,
+        'start:browser': `run-p watch:browser srv:browser`,
+        'start:node': `run-s watch:log`,
+        'test': `run-p test:*`,
+        'test:lint': `eslint ${srcDir}`,
+        'test:tape': `run-s repl -- ${testDir}/**/*.js | tap-spec`,
+        'watch:browser': `npm -s run watch:ts -- --onSuccess 'npm run-s build:browser'`,
+        'watch:log': `npm -s run watch:ts -- --onSuccess 'npm run-s log'`,
+        'watch:ts': `ts-watch ${tscArgs}`,
     },
-}
+};
