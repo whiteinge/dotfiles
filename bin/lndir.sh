14275469a09ad40f12941f665b066cc547935328
Seth House <seth@eseth.com>
2018-07-15 00:10:06 -0600

    Update lndir.sh to work with busybox and not require zsh

--- bin/lndir.sh
+++ bin/lndir.sh
@@ -1,12 +1,32 @@
-#!/usr/bin/env zsh
-# Dead-simple replacement for lndir for WSL.
+#!/usr/bin/env sh
+# Dead-simple replacement for lndir for WSL and/or busybox.
 # https://github.com/Microsoft/WSL/issues/2229
-# Attempts to avoid forking any more than necessary, but is still slow.
 
 local src="${1:?Source path is required.}"
 local dst="${2:?Destination path is required.}"
 
-find "$src" -mindepth 1 \( -name .git -type d \) -prune -o -type d -printf "${dst}/%P\0" \
-    | xargs -0 mkdir -p
-find "$src" \( -name .git -type d \) -prune -o -type d -printf '%P\0' \
-    | xargs -t -0 -I@ zsh -o GLOB_DOTS -c "ln -sf -t ${dst}/@ ${src}/@/*(.)"
+case $src in
+    /*) src=$src;;
+    *) printf 'Must be an absolute path\n'; exit 1;;
+esac
+
+find "$src" -mindepth 1 \( -name .git -type d \) -prune -o -type d \
+    | awk -v src="$src" -v dst="$dst" '
+        BEGIN { sub(/\/$/, "", src) }
+        {
+            path = substr($0, length(src) + 2)
+            print dst "/" path
+        }
+    ' \
+    | xargs mkdir -p
+
+find "$src" -mindepth 1 \( -name .git \) -prune -o -type f -print \
+    | awk -v src="$src" -v dst="$dst" '
+        BEGIN { sub(/\/$/, "", src) }
+        {
+            path = substr($0, length(src) + 2)
+            print src "/" path
+            print dst "/" path
+        }
+    ' \
+    | xargs -n 2 sh -c 'ln -sf $1 $2' -
